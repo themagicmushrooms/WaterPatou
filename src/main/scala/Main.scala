192249a6bd21b1a7a156cf55068611e90142f0aa
Seb <>
2017-08-09 19:17:49 +0200

    Add dumb archimedes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,13 +4,11 @@ import com.badlogic.gdx.backends.lwjgl._
 import com.badlogic.gdx.graphics.VertexAttributes.Usage
 import com.badlogic.gdx.{Game, Gdx}
 import com.badlogic.gdx.graphics.{Color, GL20, PerspectiveCamera, Texture}
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g3d.attributes.{BlendingAttribute, ColorAttribute}
 import com.badlogic.gdx.graphics.g3d._
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
 import com.badlogic.gdx.graphics.g3d.utils.{CameraInputController, ModelBuilder}
 import com.badlogic.gdx.math.{Plane, Vector3}
-import com.badlogic.gdx.physics.bullet.Bullet
 import com.badlogic.gdx.physics.bullet.collision._
 import com.badlogic.gdx.physics.bullet.dynamics._
 
@@ -26,24 +24,13 @@ class MyGame extends Game {
   var partShape: btCollisionShape = null
   var partBody: btRigidBody = null
 
-  var broadPhase: btBroadphaseInterface = null
-  var collisionConfig: btCollisionConfiguration = null
-  var dispatcher: btDispatcher = null
-  var world: btDynamicsWorld = null
-  var solver: btConstraintSolver = null
+  var physics: Physics = null
 
   var waterModel: Model = null
   var waterInstance: ModelInstance = null
 
   override def create() = {
-    Bullet.init()
-    broadPhase = new btDbvtBroadphase
-    collisionConfig = new btDefaultCollisionConfiguration
-    dispatcher = new btCollisionDispatcher(collisionConfig)
-    solver = new btSequentialImpulseConstraintSolver
-    world = new btDiscreteDynamicsWorld(dispatcher, broadPhase, solver, collisionConfig)
-    world.setGravity(new Vector3(0f, -10f, 0f))
-
+    physics = new Physics
     modelBatch = new ModelBatch
     cam = createCam
 
@@ -52,12 +39,13 @@ class MyGame extends Game {
       new Material(ColorAttribute.createDiffuse(Color.GREEN)),
       Usage.Position | Usage.Normal)
     partModelInstance = new ModelInstance(partModel)
+    partModelInstance.transform.translate(0f, 12f, 0f)
     partShape = new btBoxShape(new Vector3(2.5f, 2.5f, 2.5f))
     val ci = new btRigidBody.btRigidBodyConstructionInfo(125f, null, partShape)
     partBody = new btRigidBody(ci)
     partBody.setCollisionShape(partShape)
     partBody.setWorldTransform(partModelInstance.transform)
-    world.addRigidBody(partBody)
+    physics.add(partBody)
 
     waterModel = builder.createRect (
       -1f, 0f, -1f,
@@ -67,7 +55,7 @@ class MyGame extends Game {
       0f, 1f, 0f,
       new Material(
         ColorAttribute.createDiffuse(Color.BLUE),
-        new BlendingAttribute(true, 0.2f)),
+        new BlendingAttribute(true, 0.6f)),
       Usage.Position | Usage.Normal)
     waterInstance = new ModelInstance(waterModel)
     waterInstance.transform.scale(100f, 100f, 100f)
@@ -91,7 +79,7 @@ class MyGame extends Game {
   override def render() = {
 
     val delta = Math.min(1f / 30f, Gdx.graphics.getDeltaTime)
-    world.stepSimulation(delta, 5, 1f / 60f)
+    physics.step(delta)
 
     partBody.getWorldTransform(partModelInstance.transform)
 
---
+++ src/main/scala/Physics.scala
@@ -0,0 +1,46 @@
+package fr.game.pkg
+
+import com.badlogic.gdx.math.{Plane, Vector3}
+import com.badlogic.gdx.physics.bullet.Bullet
+import com.badlogic.gdx.physics.bullet.collision._
+import com.badlogic.gdx.physics.bullet.dynamics._
+import com.badlogic.gdx.utils.Disposable
+
+import scala.collection.mutable.ArrayBuffer
+
+class Physics extends Disposable {
+
+  Bullet.init()
+  var broadPhase = new btDbvtBroadphase
+  var collisionConfig = new btDefaultCollisionConfiguration
+  var dispatcher = new btCollisionDispatcher(collisionConfig)
+  var solver = new btSequentialImpulseConstraintSolver
+  var world = new btDiscreteDynamicsWorld(dispatcher, broadPhase, solver, collisionConfig)
+  world.setGravity(new Vector3(0f, -10f, 0f))
+
+  private val bodies = ArrayBuffer.empty[btRigidBody]
+
+  def add(body: btRigidBody) {
+    world.addRigidBody(body)
+    bodies.append(body)
+  }
+
+  def step(delta: Float) = {
+    world.stepSimulation(delta, 5, 1f / 60f)
+    bodies.filter(isUnderwater).foreach(applyArchimedes)
+  }
+
+  def isUnderwater(body: btRigidBody) = {
+    val v = new Vector3
+    body.getWorldTransform.getTranslation(v)
+    v.y < 0f
+  }
+
+  def applyArchimedes(body: btRigidBody) = {
+    body.applyCentralForce(new Vector3(0f, 2000f, 0f))
+  }
+
+  override def dispose() = {
+  }
+
+}
