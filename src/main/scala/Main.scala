b31cc867bffce0fdf4574834d4884f4bb6d4863a
Seb <>
2017-08-09 14:38:58 +0200

    Add a camera

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,8 +6,9 @@ import com.badlogic.gdx.{Game, Gdx}
 import com.badlogic.gdx.graphics.{Color, GL20, PerspectiveCamera, Texture}
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
-import com.badlogic.gdx.graphics.g3d.{Material, Model, ModelBatch, ModelInstance}
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
+import com.badlogic.gdx.graphics.g3d._
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
+import com.badlogic.gdx.graphics.g3d.utils.{CameraInputController, ModelBuilder}
 
 class MyGame extends Game {
 
@@ -15,28 +16,36 @@ class MyGame extends Game {
   var modelBatch: ModelBatch = null
   var img: Texture = null
   var cam: PerspectiveCamera = null
+  var camControl: CameraInputController = null
   var model: Model = null
   var modelInstance: ModelInstance = null
+  var envir: Environment = null
 
   override def create() = {
     batch = new SpriteBatch
     modelBatch = new ModelBatch
     img = new Texture("data/badlogic.jpg")
-    setupCam
+    cam = createCam
     val builder = new ModelBuilder
     model = builder.createBox(5f, 5f, 5f,
       new Material(ColorAttribute.createDiffuse(Color.GREEN)),
       Usage.Position | Usage.Normal)
     modelInstance = new ModelInstance(model)
+    envir = new Environment
+    envir.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f))
+    envir.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f))
+    camControl = new CameraInputController(cam)
+    Gdx.input.setInputProcessor(camControl)
   }
 
-  private def setupCam = {
-    cam = new PerspectiveCamera(90, Gdx.graphics.getWidth, Gdx.graphics.getHeight)
+  private def createCam = {
+    val cam = new PerspectiveCamera(90, Gdx.graphics.getWidth, Gdx.graphics.getHeight)
     cam.position.set(10f, 10f, 10f)
     cam.lookAt(0f, 0f, 0f)
     cam.near = 1f
     cam.far = 300f
     cam.update()
+    cam
   }
 
   override def render() = {
@@ -50,8 +59,9 @@ class MyGame extends Game {
     Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth, Gdx.graphics.getHeight)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT)
     modelBatch.begin(cam)
-    modelBatch.render(modelInstance)
+    modelBatch.render(modelInstance, envir)
     modelBatch.end()
+    camControl.update()
   }
 
   override def dispose() = {
